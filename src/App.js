01a0d432bfffe5bd559f46af9e54a8a8025d22e7
jaspia-1 <aspia2001.icloud@gmail.com>
2022-11-11 10:12:14 +0600

    routing done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from './Layout/Main';
+import Home from './Components/Home/Home';
+import Services from './Components/Services/Services';
+import ServiceDetails from './Components/ServiceDetails/ServiceDetails';
+import Login from './Components/Login/Login';
+import SIgnUP from './Components/SignUp/SIgnUP';
+import Myreviews from './Components/Myreviews/Myreviews';
+import AddService from './Components/AddService/AddService';
+import BLog from './Components/Blog/BLog';
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+
+        },
+        {
+          path: '/services',
+          element: <Services></Services>
+
+        },
+        {
+          path: '/services/:id',
+          element: <ServiceDetails></ServiceDetails>
+
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
+        },
+        {
+          path: '/signup',
+          element: <SIgnUP></SIgnUP>
+        },
+
+        {
+          path: '/myreview',
+          element: <Myreviews></Myreviews>
+        },
+        {
+          path: '/addservice',
+          element: <AddService></AddService>
+        },
+        {
+          path: '/blog',
+          element: <BLog></BLog>
+        }
+      ]
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/Components/SignUp/SIgnUP.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SIgnUP = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SIgnUP;
