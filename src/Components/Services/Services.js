f6b3a960bc336670ec1fbd0525b62f863c87bdcd
jaspia-1 <aspia2001.icloud@gmail.com>
2022-11-11 11:50:54 +0600

    services done

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import useTitle from '../../hooks/useTitle/useTitle';
+import ShowCards from '../Shared/ShowCards/ShowCards';
 
 const Services = () => {
+    const [services, setSerVices] = useState([]);
+
+    useTitle("Services - Aspia's Kitchen")
+    useEffect(() => {
+
+        fetch(`http://localhost:5000/services`)
+            .then(res => res.json())
+            .then(data => {
+                setSerVices(data)
+
+            })
+    }, [])
+
+
+
     return (
-        <div>
+        <div className='services'>
+            <Container style={{ maxWidth: "900px" }}>
+                <Row className='w-100' >
+                    <Col >
+
+                        <h2 className='my-3 fw-bold display-3'>Services</h2>
+                        <Row className='g-4 py-5' xs={1} md={2} lg={3}>
+                            {
+                                services.map(service => <ShowCards key={service._id} service={service}></ShowCards>)
+                            }
+
+                        </Row>
+                    </Col>
+
+                </Row>
+            </Container>
 
         </div>
     );
--- src/Components/Shared/NavHeader.js
+++ src/Components/Shared/NavHeader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavHeader = () => {
     return (
@@ -12,7 +12,7 @@ const NavHeader = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
+                            <NavLink to='/services' className="nav-link">Services</NavLink>
                             <Nav.Link href="#link">Link</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
